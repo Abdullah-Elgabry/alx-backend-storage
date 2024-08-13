c373ff98fd06e2d84895cc9b9c2a07b4b61f95a3
“abdullah <abdullah.h.elgabry@gmail.com>
2024-08-13 19:04:15 +0300

    Nosql #solved tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Top students'''
+
+
+def top_students(mongo_collection):
+    '''this func that returns all students sorted by avg score'''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
